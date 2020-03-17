51e5e6577f06abe30bffedb876de300fb0a023c6
Josef Vacek <vacekj@outlook.com>
2020-03-17 14:39:06 +0100

    Updates

---
+++ precache-manifest.42327d3adbead9f935ca8c16d7c31ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "458233fb0b19b2800e751d5575e1fe2c",
+    "url": "/jouzalol.github.io/timesaver/index.html"
+  },
+  {
+    "revision": "890164be08ead869aab8",
+    "url": "/jouzalol.github.io/timesaver/static/css/main.49da5464.chunk.css"
+  },
+  {
+    "revision": "577f34352368e96ad821",
+    "url": "/jouzalol.github.io/timesaver/static/js/2.8a6c4ba8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/jouzalol.github.io/timesaver/static/js/2.8a6c4ba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890164be08ead869aab8",
+    "url": "/jouzalol.github.io/timesaver/static/js/main.855a693c.chunk.js"
+  },
+  {
+    "revision": "3fe06041da5504cc10b2",
+    "url": "/jouzalol.github.io/timesaver/static/js/runtime-main.5a227618.js"
+  }
+]);
